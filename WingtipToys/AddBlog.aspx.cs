8b855d97203a09bc2a2086ae457e4d547e6b198b
Burrypony <kiril.chernysh@gmail.com>
2018-12-09 22:00:39 +0200

    first commit

---
+++ WingtipToys/AddBlog.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WingtipToys
+{
+    public partial class AddBlog : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void AddPost(object sender, EventArgs e)
+        {
+            if (titlePost.Text != "" && textPost.Text != "")
+            {
+                string serverConnectiob = WebConfigurationManager.ConnectionStrings["WingtipToys"].ConnectionString;
+                String queryString = "Insert into Posts(title, text, date) " +
+                                    "VALUES (@a, @b, @c )";
+
+                using (SqlConnection connection = new SqlConnection(
+                        serverConnectiob))
+                {
+                    SqlCommand command = new SqlCommand(queryString, connection);
+                    command.Parameters.AddWithValue("@a", titlePost.Text);
+                    command.Parameters.AddWithValue("@b", textPost.Text);
+                    command.Parameters.AddWithValue("@c", DateTime.Now);
+                    command.Connection.Open();
+                    command.ExecuteNonQuery();
+                    command.Connection.Close();
+                }
+            }
+        }
+    }
+}
